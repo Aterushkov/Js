397144ef79eb0ded8ecb2838e90c2c1009e8ee89
Aterushkov <Sanek.s88@gmail.com>
2020-04-26 00:18:17 +0500

    dragBlock

---
+++ DragBlock/js/script.js
@@ -0,0 +1,65 @@
+
+
+function getScroll(w) {
+	w = w || window;
+	
+	if(w.pageXOffset != null) {
+		return {x:w.pageXOffset,y:w.pageYOffset}
+	}
+}
+
+function drag (el,event) {
+	var scroll = getScroll();
+	
+	var startX = event.clientX + scroll.x;
+	var startY = event.clientY + scroll.y;
+	
+	var elX = el.offsetLeft;
+	var elY = el.offsetTop;
+	
+	var deltaX = startX - elX;
+	var deltaY = startY - elY;
+	
+	if(document.addEventListener) {
+		document.addEventListener('mousemove',moveHandler,true);
+		document.addEventListener('mouseup',upHandler,true);
+	}
+	
+	if(event.stopPropagation) {
+		event.stopPropagation();
+	}
+	if(event.preventDefault) {
+		event.preventDefault();
+	}
+	
+	function moveHandler (e) {
+		var scroll = getScroll();
+		el.style.left = (e.clientX + scroll.x - deltaX) + 'px';
+		el.style.top = (e.clientY + scroll.x - deltaY) + 'px';
+		
+		if(e.stopPropagation) {
+			e.stopPropagation();
+		}
+	}
+	function upHandler (e) {
+		if (document.removeEventListener) { 
+			document.removeEventListener("mouseup", upHandler, true);
+			document.removeEventListener("mousemove", moveHandler, true);
+		}
+		
+		if(e.stopPropagation) {
+			e.stopPropagation();
+		}
+	}
+	
+}
+
+
+
+		
+
+
+
+	
+
+	
